b0fc7a570d93f479346ea55bbd19e8417bb3c3f2
Sixophrenia <Sixophrenia@gmail.com>
2018-04-18 05:34:00 -0400

    FOoter link was broken

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 		<section id="final-section">
-			<a id="smart-contract-link" href="https://etherscan.io/address/0x49c2b0b29edfabee21311e7cff3a0c67da9f035e" target="_blank">Smart Contract</a>
+			<a id="smart-contract-link" href="https://etherscan.io/address/0x4c17c61ce6edd113346d993aed193dda7ae57b9e" target="_blank">Smart Contract</a>
 		</section>
 		</template>
 	<style lang="scss" scoped>
