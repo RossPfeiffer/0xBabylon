a024f94cbb0715252cf0ca5db7ea9d962bcc98c8
Sixophrenia <Sixophrenia@gmail.com>
2018-04-18 00:19:32 -0400

    ...

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
 		<section id="final-section">
-			<a id="smart-contract-link" href="https://etherscan.io/address/0x72088c9767f55504157159fab3fa1de8d79bbe3e" target="_blank">Smart Contract</a>
+			<a id="smart-contract-link" href="https://etherscan.io/address/0x49c2b0b29edfabee21311e7cff3a0c67da9f035e" target="_blank">Smart Contract</a>
 		</section>
 		</template>
 	<style lang="scss" scoped>
